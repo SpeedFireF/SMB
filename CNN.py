ab12778ffd2ba1b5cdbb6a1eff4ca278b9d9515d
Damir <70011787+SpeedFireF@users.noreply.github.com>
2023-05-03 20:13:47 +0300

    Add files via upload

---
+++ CNN.py
@@ -0,0 +1,30 @@
+import torch
+import torch.nn as nn
+
+class CNN(nn.Module):
+    def __init__(self, action_size, learning_rate):
+        super(CNN, self).__init__()
+        self.action_size = action_size
+        self.learning_rate = learning_rate
+
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=32, kernel_size=8, stride=4)
+        self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=4, stride=2)
+        self.conv3 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1)
+        self.flatten = nn.Flatten()
+        self.fc1 = nn.Linear(64 * 7 * 7, 512)
+        self.fc2 = nn.Linear(512, self.action_size)
+
+        self.relu = nn.ReLU()
+        self.softmax = nn.Softmax(dim=1)
+
+        self.optimizer = torch.optim.Adam(self.parameters(), lr=self.learning_rate)
+        self.loss_fn = nn.MSELoss()
+
+    def forward(self, x):
+        x = self.relu(self.conv1(x))
+        x = self.relu(self.conv2(x))
+        x = self.relu(self.conv3(x))
+        x = self.flatten(x)
+        x = self.relu(self.fc1(x))
+        x = self.softmax(self.fc2(x))
+        return x
